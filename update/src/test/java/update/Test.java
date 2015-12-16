b555fe04c8fbe7868c9524c1a60328d10d3a6134
zuoguodong <zuoguodong@91nongye.com>
2015-12-16 16:44:02 +0800

    我还原了6步，并且做了修改

--- update/src/test/java/update/Test.java
+++ update/src/test/java/update/Test.java
@@ -2,7 +2,7 @@ package update;
 
 public class Test {
 	public static void main(String args[]){
-		System.out.println("我在这里做了修改");
+		System.out.println("我在这里做了修改 我还原了6步，并且做了修改");
 		System.out.println("我在这里加了一句话");
 	}
 }

e27f11dd42a573fa14e89b81d18fc6e672fc9484
zuoguodong <zuoguodong@91nongye.com>
2015-12-16 16:27:41 +0800

    添加了工作区2

--- update/src/test/java/update/Test.java
+++ update/src/test/java/update/Test.java
@@ -2,7 +2,7 @@ package update;
 
 public class Test {
 	public static void main(String args[]){
-		System.out.println("我又在这里做了修改");
+		System.out.println("我是工作区2，我在这里做了修改");
 		System.out.println("我在这里加了一句话");
 	}
 }

0f6afc68819aad62a57dd93d8a8efab165b711d3
zuoguodong <zuoguodong@91nongye.com>
2015-12-16 15:42:50 +0800

    abcde

--- update/src/test/java/update/Test.java
+++ update/src/test/java/update/Test.java
@@ -2,7 +2,7 @@ package update;
 
 public class Test {
 	public static void main(String args[]){
-		System.out.println("abcde master 1B 1");
+		System.out.println("abcde master ababaa");
 		System.out.println("我在这里加了一句话");
 	}
 }

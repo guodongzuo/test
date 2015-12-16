941a83fa12798a375241e27f7a16d2f5af415743
zuoguodong <zuoguodong@91nongye.com>
2015-12-16 16:49:10 +0800

    我在这里做了修改提交了

--- update/src/test/java/update/Test.java
+++ update/src/test/java/update/Test.java
@@ -2,6 +2,7 @@ package update;
 
 public class Test {
 	public static void main(String args[]){
+		System.out.println("我在这里做了一次修改，提交了");
 		System.out.println("我在这里做了修改 我还原了6步，并且做了修改");
 		System.out.println("我是工作区1，我想再做一次修改，在这里做了修改");
 		System.out.println("我是工作区2，我知道工作区1也在这里做了修改，我在这里做了修改");

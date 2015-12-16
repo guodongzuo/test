90ed3bbd4b71cb5c8701b6469aaf3a2deca1f40c
zuoguodong <zuoguodong@91nongye.com>
2015-12-16 16:35:00 +0800

    我想再做一次修改

--- update/src/test/java/update/Test.java
+++ update/src/test/java/update/Test.java
@@ -2,7 +2,7 @@ package update;
 
 public class Test {
 	public static void main(String args[]){
-		System.out.println("我是工作区1，在这里做了修改");
+		System.out.println("我是工作区1，我想再做一次修改，在这里做了修改");
 		System.out.println("我在这里加了一句话");
 	}
 }
